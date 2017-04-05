90d92d4919f21d007212e9f6b2d227831e7a5187
norlindah <norlindah@mampunet.btm>
2017-04-05 15:44:15 +0800

    Course Project 2

--- Plot2.R
+++ Plot2.R
@@ -13,7 +13,7 @@ NEIBaltimore <- NEI[NEI$fips=="24510",]
 AggBaltimore <- aggregate(Emissions ~ year, NEIBaltimore, sum)
   
 png('plot2.png')
-xx <- barplot(height = round(AggBaltimore$Emissions,2), names.arg=AggBaltimore$year, xlab="Years", ylab=("PM2.5 Emissions In Tons"), main=("Total PM2.5 Emissions From Baltimore City, Maryland Sources"))
+xx <- barplot(height = round(AggBaltimore$Emissions,2), names.arg=AggBaltimore$year, xlab="Year", ylab=("PM2.5 Emissions In Tons"), main=("Total PM2.5 Emissions From Baltimore City, Maryland Sources"))
 
 text(x = xx, y = round(AggBaltimore$Emissions,2), label = round(AggBaltimore$Emissions,2), pos = 1, cex = 0.8, col = "blue")
 dev.off()  
